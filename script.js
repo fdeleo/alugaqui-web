8a80eda34932d41d3ab55ed54fc2529d64ca9d0e
Silvino Vieira <silvinov@gmail.com>
2017-05-23 15:02:23 -0300

    Implementa cadastro de corretor para o localhost

---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+    $( "#formCorretor" ).submit(function( event ) {
+
+        var corretor = {};
+        corretor['email'] = $( "#email" ).val();
+        corretor['nome'] = $( "#nome" ).val();
+        corretor['sobrenome'] = $( "#sobrenome" ).val();
+        corretor['celular'] = $( "#celular" ).val();
+        corretor['creci'] = $( "#creci" ).val();
+
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:8080/corretor",
+            data:JSON.stringify(corretor),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function(){
+                alert("Cadastro realizado");
+                $( "#submitCorretor" ).prop('disabled', false);
+                $( "#formCorretor" ).reset();
+
+            },
+            failure: function(errMsg) {alert(errMsg);}
+        });
+
+        $( "#submitCorretor" ).prop('disabled', true);
+
+        event.preventDefault();
+    });
+
+});
